8118f405f8bb62cb52826ee588eb14391f381755
Vignesh.Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2024-02-10 23:07:12 +0100

    appended local package directory to conf through relative paths

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(f'..{os.sep}..'))
+sys.path.insert(0, os.path.abspath(f'..{os.sep}..{os.sep}..'))
 
 
 # -- Project information -----------------------------------------------------
