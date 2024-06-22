888150496a21dd8df7ad6d4ea994b0f7878760e8
Vignesh.Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2024-06-22 08:37:02 +0200

    removed webdashboard namespace

--- hololinked/webdashboard/constants.py
+++
@@ -1,4 +0,0 @@
-allowedRequestMethods        = ['get', 'post', 'put', 'delete', 'options']
-INVALID_PROP                 = "INVALID_PROP"
-allowedEffectiveValueTypes   = [None, bool, int, float, str, 'htmlid']
-url_regex : str    = r'[\-a-zA-Z0-9@:%._\/\+~#=]{1,256}' 
