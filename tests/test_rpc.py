4da802690d2dc75164a3fcbe758c80d318e74624
Vignesh Venkatasubramanian Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2024-07-14 08:24:42 +0200

    added TCP to mixin as well just for sake of completeness

--- tests/test_rpc.py
+++ tests/test_rpc.py
@@ -20,10 +20,12 @@ class TestRPC(TestCase):
                         thing_cls=self.thing_cls, 
                         instance_name='test-rpc',
                         log_level=logging.WARN,
+                        protocols=['IPC', 'TCP'],
+                        tcp_socket_address='tcp://*:60000',
                         http_server=True
                     )   
         self.thing_client = ObjectProxy('test-rpc') # type: TestThing
-
+       
     @classmethod
     def tearDownClass(self):
         self.thing_client.exit()
@@ -60,8 +62,14 @@ class TestRPC(TestCase):
         start_client(done_queue, 'http')
         self.assertEqual(done_queue.get(), True)
 
+    def test_6_tcp_client(self):
+        # Also, for sake, a TCP client
+        done_queue = multiprocessing.Queue()
+        start_client(done_queue, tcp_socket_address='tcp://localhost:60000')
+        self.assertEqual(done_queue.get(), True)
 
-    def test_6_multiple_clients(self):
+
+    def test_7_multiple_clients(self):
         # Then parallely run all of them at once and make sure they all succeed
         # which means the server can request accept from anywhere at any time and not fail
         done_queue_1 = multiprocessing.Queue()
@@ -81,21 +89,29 @@ class TestRPC(TestCase):
 
         done_queue_6 = multiprocessing.Queue()
         start_client(done_queue_6, 'http')
-       
+
+        done_queue_7 = multiprocessing.Queue()
+        start_client(done_queue_7, typ='threading', tcp_socket_address='tcp://localhost:60000')
+
+        done_queue_8 = multiprocessing.Queue()
+        start_client(done_queue_8, tcp_socket_address='tcp://localhost:60000')
+
         self.assertEqual(done_queue_1.get(), True)
         self.assertEqual(done_queue_2.get(), True)
         self.assertEqual(done_queue_3.get(), True)
         self.assertEqual(done_queue_4.get(), True)
         self.assertEqual(done_queue_5.get(), True)
         self.assertEqual(done_queue_6.get(), True)
+        self.assertEqual(done_queue_7.get(), True)
+        self.assertEqual(done_queue_8.get(), True)
 
 
 
-def start_client(done_queue : multiprocessing.Queue, typ : str = 'normal'):
+def start_client(done_queue : multiprocessing.Queue, typ : str = 'normal', tcp_socket_address : str = None):
     if typ == 'normal':
-        return multiprocessing.Process(target=normal_client, args=(done_queue,)).start()
+        return multiprocessing.Process(target=normal_client, args=(done_queue, tcp_socket_address)).start()
     elif typ == 'threading':
-        return multiprocessing.Process(target=threading_client, args=(done_queue,)).start()
+        return multiprocessing.Process(target=threading_client, args=(done_queue, tcp_socket_address)).start()
     elif typ == 'async':
         return multiprocessing.Process(target=async_client, args=(done_queue,)).start()
     elif typ == 'async_multiple':
@@ -114,14 +130,18 @@ def gen_random_data():
                         None])
 
 
-def normal_client(done_queue : multiprocessing.Queue = None):
+def normal_client(done_queue : multiprocessing.Queue = None, tcp_socket_address : str = None):
     success = True
-    client = ObjectProxy('test-rpc') # type: TestThing
+    if tcp_socket_address:
+        client = ObjectProxy('test-rpc', socket_address=tcp_socket_address, protocol='TCP') # type: TestThing
+    else:
+        client = ObjectProxy('test-rpc') # type: TestThing
     for i in range(2000):
         value = gen_random_data()
         ret = client.test_echo(value)
         # print("single-thread", 1, i, value, ret)
-        if value != ret:
+        if value != ret:    
+            print("error", "single-thread", 1, i, value, ret)
             success = False
             break
 
@@ -129,9 +149,12 @@ def normal_client(done_queue : multiprocessing.Queue = None):
         done_queue.put(success)
 
    
-def threading_client(done_queue : multiprocessing.Queue = None):
+def threading_client(done_queue : multiprocessing.Queue = None, tcp_socket_address : str = None):
     success = True
-    client = ObjectProxy('test-rpc') # type: TestThing
+    if tcp_socket_address:
+        client = ObjectProxy('test-rpc', socket_address=tcp_socket_address, protocol='TCP') # type: TestThing
+    else:
+        client = ObjectProxy('test-rpc') # type: TestThing
 
     def message_thread(id : int):
         nonlocal success, client
@@ -140,6 +163,7 @@ def threading_client(done_queue : multiprocessing.Queue = None):
             ret = client.test_echo(value)
             # print("multi-threaded", id, i, value, ret)
             if value != ret:
+                print("error", "multi-threaded", 1, i, value, ret)
                 success = False
                 break
 
@@ -168,6 +192,7 @@ def async_client(done_queue : multiprocessing.Queue = None):
             ret = await client.async_invoke('test_echo', value)
             # print("async", 1, i, value, ret)
             if value != ret:
+                print("error", "async", 1, i, value, ret)
                 success = False
                 break
 
@@ -187,6 +212,7 @@ def async_client_multiple(done_queue : multiprocessing.Queue = None):
             ret = await client.async_invoke('test_echo', value)
             # print("multi-coro", id, i, value, ret)
             if value != ret:
+                print("error", "multi-coro", id, i, value, ret)
                 success = False
                 break
 
@@ -201,6 +227,7 @@ def http_client(done_queue : multiprocessing.Queue = None):
     session = requests.Session()
 
     def worker(id : int):
+        nonlocal success
         for i in range(1000):
             value = gen_random_data()
             ret =  session.post(
@@ -210,6 +237,7 @@ def http_client(done_queue : multiprocessing.Queue = None):
                         )       
             # print("http", id, i, value, ret)
             if value != ret.json():
+                print("http", id, i, value, ret)
                 success = False
                 break
             
